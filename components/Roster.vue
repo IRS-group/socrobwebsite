7321f252241e6fcfadf161622e1570d8deffad8f
Rodrigo Serra <serra.rfs@gmail.com>
2024-10-29 17:02:08 +0000

    Finished Roster section

--- components/Roster.vue
+++ components/Roster.vue
@@ -1,6 +1,9 @@
 <template>
     <section class="roster section" id="roster">
       <h2 class="section__title">Roster</h2>
+      <span class="section__subtitle">
+        <v-icon class="roster_icon">mdi-account-group-outline</v-icon>
+      </span>
       <v-container class="roster__container" fluid>
         <v-row justify="center">
           <v-col v-for="member in teamMembers" :key="member.name" cols="12" sm="6" md="4">
@@ -22,62 +25,80 @@
     {
       name: 'Pedro Lima',
       position: 'Project Coordinator',
-      image: '/team/pedroLima.jpg',
+      image: '/team/cropped/pedroLima.png',
     },
     {
       name: 'Rodrigo Serra',
       position: 'Team Leader',
-      image: '/team/rodrigo.png',
+      image: '/team/cropped/rodrigo.png',
     },
     {
       name: 'Rui Bettencourt',
       position: 'PhD Student',
-      image: '/team/rui.png',
+      image: '/team/cropped/rui.png',
     },
     {
       name: 'Gabriel Nunes',
       position: 'Hardware Tech',
-      image: '/team/gabrielNunes.JPG',
+      image: '/team/cropped/gabrielNunes.png',
     },
     {
       name: 'Afonso Certo',
       position: 'MSc Student',
-      image: '/team/afonsoCerto.png',
+      image: '/team/cropped/afonsoCerto.png',
     },
     {
       name: 'Miguel Nabais',
       position: 'MSc Student',
-      image: '/team/miguelNabais.png',
+      image: '/team/cropped/miguelNabais.png',
+    },
+    {
+      name: 'Catarina Caramalho',
+      position: 'MSc Student',
+      image: '/team/cropped/catarinaCaramalho.png',
+    },
+    {
+      name: 'Patrícia Torres',
+      position: 'MSc Student',
+      image: '/team/cropped/patriciaTorres.png',
+    },
+    {
+      name: 'António Morais',
+      position: 'MSc Student',
+      image: '/team/cropped/antonioMorais.png',
+    },
+    {
+      name: 'Robin Steiger',
+      position: 'MSc Student',
+      image: '/team/cropped/robinSteiger.png',
+    },
+    {
+      name: 'Kevin Alcedo',
+      position: 'PhD Student',
+      image: '/team/kevinAlcedo.png',
+    },
+    {
+      name: 'Rodrigo Coimbra',
+      position: 'MSc Student',
+      image: '/team/robotMale.jpg',
+    },
+    {
+      name: 'Teresa Nogueira',
+      position: 'MSc Student',
+      image: '/team/robotFemale.jpg',
+    },
+    {
+      name: 'André Silva',
+      position: 'Research Engineer',
+      image: '/team/robotMale.jpg',
+    },
+    {
+      name: 'Matilde Vital',
+      position: 'MSc Student',
+      image: '/team/robotFemale.jpg',
     },
   ];
   </script>
   
-  <style scoped>
-  .roster {
-    text-align: center;
-    padding: 2rem 0;
-  }
-  
-  .roster__container {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
-  
-  .roster__info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 1rem;
-  }
-  
-  .roster__name {
-    font-weight: bold;
-    margin-top: 0.5rem;
-  }
-  
-  .roster__position {
-    color: gray;
-    margin-top: 0.25rem;
-  }
-  </style>
+  <style scoped lang="scss" src="@/assets/roster.scss"></style>
   
